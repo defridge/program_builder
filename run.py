b77bdb2e272046401476d61af6fa5b9e42f43df3
defridge <>
2024-01-12 14:56:46 +0000

    Initial commit and first function added

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+
+def main_menu():
+    """
+    Displays main menu and welcome message when program is started.
+    """
+    print("***************************************\n")
+    print("WECOME TO OUR ONLINE GYM PROGRAM BUILDER.\n")
+    print("Before we get started building your program\nwe have a few quick medical questions you must answer.\n")
+
+    while True:
+        prompt = "Are you ready to proceed: (Yes/No)"
+        response = input(prompt + "\n")
+
+        try:
+            if response in ["Yes", "yes", "y", "YES", "Y"]:
+                print("Proceeding...")
+            else:
+                if response in ["No", "no", "NO", "n", "N"]:
+                    print("That's no problem! Come back when you are ready!")
+                else:
+                    print("Invalid response. Please enter 'Yes' or 'No'.")
+        except ValueError:
+            print("Invalid input. Please enter 'Yes' or 'No'.")
+
+main_menu()
